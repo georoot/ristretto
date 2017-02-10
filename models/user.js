c1eb35199f793acc73cc7ebc307d9f64eac4f96c
georoot <rb1223334444@gmail.com>
2017-02-11 03:51:48 +0530

    Change base git dir and bugs removed

--- models/user.js
+++ models/user.js
@@ -3,7 +3,11 @@ var Schema = mongoose.Schema;
 
 var userSchema = new Schema({
   username: {type: String, required: true},
-  password: {type: String, required: true}
+  password: {type: String, required: true},
+  keys    : [{
+    value : {type: String},
+    title : {type: String}
+  }]
 });
 
 module.exports = mongoose.model('user',userSchema);
