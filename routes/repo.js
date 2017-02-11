f536c30da266748dc57d37d6de5c78fa37d16547
georoot <rb1223334444@gmail.com>
2017-02-11 08:06:17 +0530

    Basic api endpoints for repo

--- routes/repo.js
+++ routes/repo.js
@@ -1,5 +1,57 @@
 const express = require('express');
-const crud    = require('../helper/crud');
+const crudHelper    = require('../helper/crud');
+const model   = require('../models/repo');
+var crud = new crudHelper(model);
 var route = express.Router();
+
+route.get("/",(req,res,next)=>{
+  crud['list']({})
+    .then((data)=>{
+      res.json(data);
+    })
+    .catch((err)=>{
+      res.status(400).json({err:err.message});
+    });
+});
+
+route.get("/:id",(req,res,next)=>{
+  crud['getOne'](req.params.id)
+    .then((data)=>{
+      res.json(data);
+    })
+    .catch((err)=>{
+      res.status(400).json({err:err.message});
+    });
+});
+
+route.post("/",(req,res,next)=>{
+  crud['add'](req.body)
+    .then(()=>{
+      res.status(200).end();
+    })
+    .catch((err)=>{
+      res.status(400).json({err:err.message});
+    });
+});
+
+route.put("/",(req,res,next)=>{
+  crud['update'](req.body)
+    .then((data)=>{
+      res.json(data);
+    })
+    .catch((err)=>{
+      res.status(400).json({err:err.message});
+    });
+});
+
+route.delete("/:id",(req,res,next)=>{
+  crud['del'](req.params.id)
+    .then(()=>{
+      res.status(200).end();
+    })
+    .catch((err)=>{
+      res.status(400).json({err:err.message});
+    });
+});
 
 module.exports = route;
