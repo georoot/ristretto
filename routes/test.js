468a5fd3ab8607d9507a233ed4d436ab197c170e
georoot <rb1223334444@gmail.com>
2017-02-11 07:37:15 +0530

    User route added

--- routes/test.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-
-var route = express.Router();
-route.get("/",(req,res,next)=>{
-  res.send("This is just a test route for api");
-});
-module.exports = route;
---
+++ routes/user.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const helper  = require('../helper/auth');
+const sec = require('../helper/security');
+
+var route = express.Router();
+
+route.post("/",(req,res,next)=>{
+  helper['hashPassword'](req.body)
+    .then(helper['setUserRole'])
+    .then(helper['save'])
+    .then(()=>{
+      res.status(200).end();
+    })
+    .catch((err)=>{
+      res.status(400).json({err:err.message});
+    });
+});
+
+route.get("/",sec['getUser'],sec['allowByRole'](["admin"]),(req,res,next)=>{
+  helper['list']()
+    .then((data)=>{
+      res.json(data);
+    })
+    .catch((err)=>{
+      res.status(400).json({err:err.message});
+    });
+});
+
+route.post("/token",(req,res,next)=>{
+  helper['findUser'](req.body)
+    .then(helper['validatePassword'])
+    .then(helper['generateToken'])
+    .then((token)=>{
+      res.json({token:token});
+    })
+    .catch((err)=>{
+      res.status(400).json({err:err.message});
+    });
+});
+
+module.exports = route;
