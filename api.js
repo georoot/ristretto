8191d20b1e328431fca62e9e31a67785de5f71cd
georoot <rb1223334444@gmail.com>
2017-02-11 07:56:48 +0530

    Body parser added

--- api.js
+++ api.js
@@ -3,12 +3,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const loader = require('require-dir');
 const chalk = require('chalk');
+const parser = require('body-parser');
 mongoose.connect('mongodb://'+process.env.DbHost+'/'+process.env.DbName);
 mongoose.Promise = global.Promise;
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 
 var app = express();
+app.use(parser.json());
 
 var routes = loader('./routes');
 for(var route in routes){
