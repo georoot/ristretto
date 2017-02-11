0c71be69771ffdd3e4ea48518a654bbf84a22738
georoot <rb1223334444@gmail.com>
2017-02-11 07:19:14 +0530

    Model change to accomodate ObjectId

--- models/repo.js
+++ models/repo.js
@@ -1,16 +1,13 @@
 const mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var objId  = Schema.ObjectId;
 
 var repoSchema = new Schema({
   title: {type: String,required: true},
   owner: {type: String, required: true},
   pub  : {type: Boolean,default: false},
-  read : [{
-    id : {type: String}
-  }],
-  write: [{
-    id: {type: String}
-  }]
+  read : {type: Array},
+  write: {type: Array}
 });
 
 module.exports = mongoose.model('repo',repoSchema);
