034af51af0e4571ba7dfd9571639c6e43ee8a74b
georoot <rb1223334444@gmail.com>
2017-02-13 21:42:56 +0530

    Bcrypt changed to bcryptjs

--- helper/auth.js
+++ helper/auth.js
@@ -1,5 +1,5 @@
 const model = require('../models/user');
-const bcrypt= require('bcrypt');
+const bcrypt= require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const dir = require('mkdirp');
 
